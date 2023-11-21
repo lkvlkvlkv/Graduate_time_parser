6da4bd393c9947ac798cf88bad81521a598939f4
LKV <a23202418@gmail.com>
2023-11-21 23:15:44 +0800

    fix: update NCTU number rule

--- controller/id_converter.py
+++ controller/id_converter.py
@@ -1,25 +1,29 @@
 def NCTU(number):
-    number = number[0:2]
-    if(number[0]=="0"):
-        number = "1" + number
+    if (number[0] == "0"):
+        number = "1" + number[0:2]
+    if (number[0] == "3" or number[0] == "5"):
+        number = "1" + number[1:3]
     return number
+
 
 def NCU(number):
-    if(number[0]=="1"):
+    if (number[0] == "1"):
         number = number[0:3]
     else:
         number = number[0:2]
     return number
 
+
 def NTHU(number):
-    if(number[0]=="1"):
+    if (number[0] == "1"):
         number = number[0:3]
     else:
         number = number[0:2]
     return number
+
 
 def NYMU(number):
     number = number[1:3]
-    if(number[0]=="0"):
+    if (number[0] == "0"):
         number = "1" + number
     return number
