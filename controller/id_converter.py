2e592ec7d308c89b951d4f8c102faacd637ce64e
LKV <a23202418@gmail.com>
2023-11-25 19:40:00 +0800

    fix: grad year < 100 will parse failed

--- controller/id_converter.py
+++ controller/id_converter.py
@@ -1,8 +1,10 @@
 def NCTU(number):
     if (number[0] == "0"):
         number = "1" + number[0:2]
-    if (number[0] == "3" or number[0] == "5"):
+    elif (number[0] == "3" or number[0] == "5"):
         number = "1" + number[1:3]
+    else:
+        number = number[0:2]
     return number
 
 
